8b7292bfbcd24c8d8763304f994d358c0d0b598e
emad <almsouti.e@gmail.com>
2021-11-06 19:52:08 +0100

    work on accordions

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import CookieConsentModal from "./CookieConsentModal";
 
 
-function App() {
-    const dummyText="We use cookies to optimize your user experience. All details can be found in ourprivacy policy."
+const App:React.FC= ()=> {
+    const dummyText="Wir verwenden Cookies und ähnliche Technologien, um Inhalte zu personalisieren, das Nutzererlebnis zu optimieren sowie um Werbung anzupassen und zu bewerten. Indem du auf “Alle akzeptieren“ klickst oder in den Einstellungen eine Option aktivierst, stimmst du dem zu. Dies wird auch in unserer Datenschutzrichtlinie beschrieben. Um deine Einstellungen zu ändern oder deine Einwilligung zu widerrufen, aktualisiere einfach deine Cookie-Einstellungen."
     return (
         <div>
             {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
             <CookieConsentModal open={true} handleClose={()=>{}}
                                 dialogTitle={'Deine Cookie-Einstellungen'}
                                 dialogContextText={dummyText}
+                                maxWidth={'md'}
             />
 
         </div>
--- src/CookieConsentModal.tsx
+++ src/CookieConsentModal.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
-import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
+import {
+    Accordion,
+    AccordionDetails, AccordionSummary, Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle, Divider, Switch, Typography
+} from "@mui/material";
 import {makeStyles,} from '@mui/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export interface CookieConsentModalProps {
     open: boolean;
@@ -11,24 +21,38 @@ export interface CookieConsentModalProps {
 
 }
 
-const useStyles = makeStyles({
+//TODO @Emad fix theme
+const useStyles = makeStyles(() => ({
     root: {},
     buttonsContainer: {
         marginRight: '16px',
+        marginTop: '16px'
     },
-    button:{
+    button: {
         marginRight: '16px',
+    },
+    accordion: {
+        boxShadow: 'none'
+
+    },
+    details: {
+        marginLeft: '200px'
     }
-});
+
+
+}));
 
 
 const CookieConsentModal: React.FC<CookieConsentModalProps> = (props) => {
     const classes = useStyles();
     return (
         <Dialog
+
             open={props.open}
             onClose={() => props.handleClose()}
             maxWidth={props.maxWidth}
+
+
         >
             <DialogTitle>{props.dialogTitle}</DialogTitle>
             <DialogContent>
@@ -36,6 +60,62 @@ const CookieConsentModal: React.FC<CookieConsentModalProps> = (props) => {
                     {props.dialogContextText}
                 </DialogContentText>
             </DialogContent>
+
+
+            <Accordion disableGutters={true} style={{boxShadow: 'none'}}>
+                <Box display={'flex'} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
+                    <AccordionSummary>
+                        <Box display={'flex'} flexDirection={'row'} alignItems={'center'}>
+                            <ExpandMoreIcon/>
+                            <Typography>Notwendig</Typography>
+                        </Box>
+
+                    </AccordionSummary>
+                    <Switch defaultChecked disabled={true}/>
+                </Box>
+
+                <AccordionDetails>
+                    <Typography>
+                        Notwendige Cookies helfen dabei, eine Webseite nutzbar zu machen, indem sie Grundfunktionen wie
+                        Seitennavigation und Zugriff auf sichere Bereiche der Webseite ermöglichen. Die Webseite kann
+                        ohne diese Cookies nicht richtig funktionieren.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion disableGutters={true} style={{boxShadow: 'none'}}>
+                <Box display={'flex'} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
+                    <AccordionSummary>
+                        <Box display={'flex'} flexDirection={'row'} alignItems={'center'}>
+                            <ExpandMoreIcon/>
+                            <Typography>Komfort</Typography>
+                        </Box>
+                    </AccordionSummary>
+                    <Switch defaultChecked/>
+                </Box>
+                <AccordionDetails>
+                    <Typography>
+                        Komfort-Cookies ermöglichen einer Webseite sich an Informationen zu erinnern, die die Art
+                        beeinflussen, wie sich eine Webseite verhält oder aussieht, wie z. B. Ihre bevorzugte Sprache
+                        oder die Region in der Sie sich befinden.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion disableGutters={true} style={{boxShadow: 'none'}}>
+                <Box display={'flex'} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
+                    <AccordionSummary>
+                        <Box display={'flex'} flexDirection={'row'} alignItems={'center'}>
+                            <ExpandMoreIcon/>
+                            <Typography>Statistik</Typography>
+                        </Box>
+                    </AccordionSummary>
+                    <Switch defaultChecked/>
+                </Box>
+                <AccordionDetails>
+                    <Typography>Statistik-Cookies helfen Webseiten-Besitzern zu verstehen, wie Besucher mit Webseiten
+                        interagieren, indem Informationen anonym gesammelt und gemeldet werden.</Typography>
+                </AccordionDetails>
+            </Accordion>
             <DialogActions>
                 <div className={classes.buttonsContainer}>
                     <Button className={classes.button}>Settings</Button>
@@ -47,5 +127,6 @@ const CookieConsentModal: React.FC<CookieConsentModalProps> = (props) => {
         </Dialog>
     )
 }
+
 
 export default CookieConsentModal;
